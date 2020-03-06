cd973a224ebb41efcdf40281af6f9d5f730a0ba3
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2020-03-06 15:53:06 +0530

    Style added.

--- viber/screens/HomeScreen.js
+++ viber/screens/HomeScreen.js
@@ -1,13 +1,23 @@
 import * as React from 'react';
+import { StyleSheet } from 'react-native'
 import { Container, Button, Text } from 'native-base';
 
 export default function HomeScreen() {
   return (
-    <Container>
+    <Container style={styles.container}>
         <Button>
           <Text>Click Me</Text>
 	</Button>
 </Container>
   );
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
 
